4df0da8ddb5284a5abf2e04b2f040e505bfa2e8e
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-05-24 11:18:34 +0530

    Time: 55 ms (33.60%), Space: 50.6 MB (6.94%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int n = nums.length;
+        Integer[] dp = new Integer[n];
+        dp[n - 1] = 0;
+        for(int i = n - 2 ; i >= 0 ; i--){
+            int steps = nums[i];
+            int min = Integer.MAX_VALUE;
+            for(int j = 1 ; j <= steps && i + j < n ; j++){
+                if(dp[i + j] != null && dp[i + j] < min){
+                    min = dp[i + j];
+                }
+            }
+            if(min != Integer.MAX_VALUE){
+                dp[i] = min + 1;
+            }
+        }
+        return dp[0];
+    }
+}

4b38808165ba62025f1d0e1921c8ba9ca1d12d6a
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-05-26 10:30:49 +0530

    Time: 11 ms (90.25%), Space: 73.1 MB (10.83%) - LeetHub

--- 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.java
+++ 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.java
@@ -2,29 +2,39 @@ class Solution {
     public String maxValue(String n, int x) {
         StringBuilder pre = new StringBuilder(n);
         int len = n.length();
-        if(n.charAt(0) != '-'){
-            int i = 0;
-            while(i < len){
-                char ch = n.charAt(i);
-                int num = ch - '0';
-                if(num < x){
-                    break;
-                }
-                i++;
+        int i = n.charAt(0) == '-' ? 1 : 0;
+        while(i < len){
+            char ch = n.charAt(i);
+            int num = ch - '0';
+            if(n.charAt(0) != '-' ? num < x : num > x){
+                break;
             }
-            pre.insert(i , x);
-        }else{
-            int i = 1;
-            while(i < len){
-                char ch = n.charAt(i);
-                int num = ch - '0';
-                if(num > x){
-                    break;
-                }
-                i++;
-            }
-            pre.insert(i , x);
+            i++;
         }
+        pre.insert(i , x);
+        // if(n.charAt(0) != '-'){
+        //     int i = 0;
+        //     while(i < len){
+        //         char ch = n.charAt(i);
+        //         int num = ch - '0';
+        //         if(num < x){
+        //             break;
+        //         }
+        //         i++;
+        //     }
+        //     pre.insert(i , x);
+        // }else{
+        //     int i = 1;
+        //     while(i < len){
+        //         char ch = n.charAt(i);
+        //         int num = ch - '0';
+        //         if(num > x){
+        //             break;
+        //         }
+        //         i++;
+        //     }
+        //     pre.insert(i , x);
+        // }
         return pre.toString();
     }
 }

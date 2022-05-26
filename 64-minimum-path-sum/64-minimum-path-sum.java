e6ce2b2273a170aeaa1a16284fe6730d0bca2dd6
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-05-26 11:41:19 +0530

    Time: 1 ms (99.69%), Space: 45 MB (81.04%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int n = grid.length , m = grid[0].length;
+        int[][] dp = new int[n + 1][m + 1];
+        for(int[] ar : dp){
+            Arrays.fill(ar , -1);
+        }
+        // return recursion1(n - 1 , m - 1 , grid);
+        // return recursion2(0 , 0 , grid);
+        return memo(n - 1 , m - 1 , grid , dp);
+    }
+    // public int recursion1(int i , int j , int[][] grid){
+    //     if(i == 0 && j == 0){
+    //         return grid[0][0];
+    //     }
+    //     if(i < 0 || j < 0){
+    //         return (int)Math.pow(10,9);
+    //     }
+    //     int up = grid[i][j] + recursion(i - 1 , j , grid);
+    //     int left = grid[i][j] + recursion(i , j - 1 , grid);
+    //     return Math.min(up , left);
+    // }
+    // public int recursion2(int i , int j , int[][] grid){
+    //     if(i == grid.length - 1 && j == grid[0].length - 1){
+    //         return grid[grid.length - 1][grid[0].length - 1];
+    //     }
+    //     if(i >= grid.length || j >= grid[0].length){
+    //         return (int)Math.pow(10,9);
+    //     }
+    //     int up = grid[i][j] + recursion2(i + 1 , j , grid);
+    //     int left = grid[i][j] + recursion2(i , j + 1 , grid);
+    //     return Math.min(up , left);
+    // }
+    public int memo(int i , int j , int[][] grid , int[][] dp){
+        if(i == 0 && j == 0){
+            return grid[0][0];
+        }
+        if(i < 0 || j < 0){
+            return (int)Math.pow(10,9);
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int up = grid[i][j] + memo(i - 1 , j , grid , dp);
+        int left = grid[i][j] + memo(i , j - 1 , grid , dp);
+        return dp[i][j] = Math.min(up , left);
+    }
+}

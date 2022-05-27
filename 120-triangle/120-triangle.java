db03a52c50d3c1872a14e2f8bcf74fe7a14d15fc
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-05-27 12:15:28 +0530

    Time: 5 ms (50.26%), Space: 44.4 MB (31.88%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -2,11 +2,22 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size() , m = triangle.get(n - 1).size();
         int[][] dp = new int[n][m];
-        for(int[] ar : dp){
-            Arrays.fill(ar , -1);
+        // for(int[] ar : dp){
+        //     Arrays.fill(ar , -1);
+        // }
+        // // return recursion(0 , 0 , triangle);
+        // return memo(0 , 0 , triangle , dp);
+        for(int j = 0 ; j < n ; j++){
+            dp[n - 1][j] = triangle.get(n - 1).get(j);
         }
-        // return recursion(0 , 0 , triangle);
-        return memo(0 , 0 , triangle , dp);
+        for(int i = n - 2 ; i >= 0 ; i--){
+            for(int j = i ; j >= 0 ; j--){
+                int down = triangle.get(i).get(j) + dp[i + 1][j];
+                int diag = triangle.get(i).get(j) + dp[i + 1][j + 1];
+                dp[i][j] = Math.min(down , diag);
+            }
+        }
+        return dp[0][0];
     }
     // public int recursion(int i , int j , List<List<Integer>> triangle){
     //     if(i == triangle.size() - 1){

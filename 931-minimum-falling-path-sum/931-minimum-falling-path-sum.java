ea989f79858baaeed4500ff5370b4a935c6ae904
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-05-28 12:17:26 +0530

    Time: 15 ms (9.43%), Space: 48.2 MB (17.74%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -2,15 +2,16 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length , m = matrix[0].length;
         int[][] dp = new int[n][m];
-        for(int[] ar : dp){
-            Arrays.fill(ar , -1);
-        }
-        int min = Integer.MAX_VALUE;
-        for(int j = 0 ; j < m ; j++){
-            int call = memo(n - 1 , j , matrix , dp);
-            min = Math.min(min , call);
-        }
-        return min;
+        // for(int[] ar : dp){
+        //     Arrays.fill(ar , -1);
+        // }
+        // int min = Integer.MAX_VALUE;
+        // for(int j = 0 ; j < m ; j++){
+        //     int call = memo(n - 1 , j , matrix , dp);
+        //     min = Math.min(min , call);
+        // }
+        // return min;
+        return tabulation(n , m , matrix , dp);
     }
     public int memo(int i , int j , int[][] m , int[][] dp){
         if(j < 0 || j >= m[0].length){
@@ -26,5 +27,28 @@ class Solution {
         int leftDiag = m[i][j] + memo(i - 1 , j - 1 , m , dp);
         int rightDiag = m[i][j] + memo(i - 1 , j + 1 , m , dp);
         return dp[i][j] = Math.min(up , Math.min(leftDiag , rightDiag));
+    }
+    public int tabulation(int n , int m , int[][] matrix , int[][] dp){
+        for(int j = 0 ; j < m ; j++){
+            dp[0][j] = matrix[0][j];
+        }
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                int left = (int)Math.pow(10 , 9) + 7 , right = (int)Math.pow(10 , 9) + 7;
+                if(j >= 1){
+                    left = matrix[i][j] + dp[i - 1][j - 1];
+                }
+                if(j + 1 < m){
+                    right = matrix[i][j] + dp[i - 1][j + 1];
+                }
+                int up = matrix[i][j] + dp[i - 1][j];
+                dp[i][j] = Math.min(up , Math.min(left , right));
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for(int j = 0 ; j < m ; j++){
+            min = Math.min(min , dp[n - 1][j]);
+        }
+        return min;
     }
 }

e0813fb8215d3570ef95b39d82aa2cd66c145719
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-02 10:47:23 +0530

    Time: 2 ms (92.83%), Space: 45.4 MB (5.58%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,41 +1,20 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size() , m = triangle.get(n - 1).size();
-        int[][] dp = new int[n][m];
-        // for(int[] ar : dp){
-        //     Arrays.fill(ar , -1);
-        // }
-        // // return recursion(0 , 0 , triangle);
-        // return memo(0 , 0 , triangle , dp);
-        for(int j = 0 ; j < n ; j++){
-            dp[n - 1][j] = triangle.get(n - 1).get(j);
+        int[][] dp = new int[triangle.size()][triangle.get(triangle.size() - 1).size()];
+        for(int[] ar : dp){
+            Arrays.fill(ar , -1);
         }
-        for(int i = n - 2 ; i >= 0 ; i--){
-            for(int j = i ; j >= 0 ; j--){
-                int down = triangle.get(i).get(j) + dp[i + 1][j];
-                int diag = triangle.get(i).get(j) + dp[i + 1][j + 1];
-                dp[i][j] = Math.min(down , diag);
-            }
-        }
-        return dp[0][0];
+        return recursion(0 , 0 , triangle , dp);
     }
-    // public int recursion(int i , int j , List<List<Integer>> triangle){
-    //     if(i == triangle.size() - 1){
-    //         return triangle.get(i).get(j);
-    //     }
-    //     int down = triangle.get(i).get(j) + recursion(i + 1 , j , triangle);
-    //     int diag = triangle.get(i).get(j) + recursion(i + 1 , j + 1 , triangle);
-    //     return Math.min(down , diag);
-    // }
-    public int memo(int i , int j , List<List<Integer>> triangle , int[][] dp){
+    public int recursion(int i , int j , List<List<Integer>> triangle , int[][] dp){
         if(i == triangle.size() - 1){
             return triangle.get(i).get(j);
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        int down = triangle.get(i).get(j) + memo(i + 1 , j , triangle , dp);
-        int diag = triangle.get(i).get(j) + memo(i + 1 , j + 1 , triangle , dp);
-        return dp[i][j] = Math.min(down , diag);
+        int down = triangle.get(i).get(j) + recursion(i + 1 , j , triangle , dp);
+        int right = triangle.get(i).get(j) + recursion(i + 1 , j + 1 , triangle , dp);
+        return dp[i][j] = Math.min(down , right);
     }
 }

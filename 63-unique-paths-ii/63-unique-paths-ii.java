2431e0c44bc9ac46dfaa24d5353458c711fcd162
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-06 09:56:26 +0530

    Time: 9 ms (5.60%), Space: 42.7 MB (21.94%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,33 +1,22 @@
 class Solution {
-    //RECURSIVE SOLUTION
-    // public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    //     int m = obstacleGrid.length , n = obstacleGrid[0].length;
-    //     return recursiveFunction(obstacleGrid , n , m , 0 , 0);
-    // }
-    // public int recursiveFunction(int[][] grid , int n , int m , int r , int c){
-    //     if(r == m || c == n || grid[r][c] == 1){
-    //         return 0;
-    //     }
-    //     if(r == m - 1 && c == n - 1){
-    //         return 1;
-    //     }
-    //     int down = recursiveFunction(grid , n , m , r + 1 , c);
-    //     int right = recursiveFunction(grid , n , m , r , c + 1);
-    //     return down + right;
-    // }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int dp[][] = new int[m][n];
-        return helper(obstacleGrid,0,0,m,n,dp);
+        int n = obstacleGrid.length , m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
+        return recursion(n - 1 , m - 1 , obstacleGrid , dp);
     }
-    public int helper(int[][] grid,int r,int c,int m,int n,int[][] dp){
-        if(r == m || c == n || grid[r][c] == 1)return 0;
-        if(r == m-1 && c == n-1){
-            if(grid[r][c] == 1)return 0;
-            return 1;
+    public int recursion(int i , int j , int[][] grid , int[][] dp){
+        if(i == 0 && j == 0){
+            if(grid[0][0] == 0) return 1;
+            return 0;
         }
-        if(dp[r][c] > 0)return dp[r][c];
-        return dp[r][c] = helper(grid,r+1,c,m,n,dp)+helper(grid,r,c+1,m,n,dp);
+        if(i < 0 || j < 0 || grid[i][j] == 1){
+            return 0;
+        }
+        if(dp[i][j] > 0){
+            return dp[i][j];
+        }
+        int up = recursion(i - 1 , j , grid , dp);
+        int left = recursion(i , j - 1 , grid , dp);
+        return dp[i][j] = up + left;
     }
 }

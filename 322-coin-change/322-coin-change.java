210af60c283081fba99d3902e6fece2411c272d5
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-09 09:59:33 +0530

    Time: 181 ms (8.40%), Space: 55.3 MB (16.89%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,70 +2,30 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[][] dp = new int[n][amount + 1];
-        // for(int[] ar : dp){
-        //     Arrays.fill(ar , -1);
-        // }
-        // // return recursion(n - 1 , amount , coins);
-        // int val = memo(n - 1 , amount , coins , dp);
-        // if(val == (int)Math.pow(10 , 9) + 7){
-        //     return -1;
-        // }
-        // return val;
-        for(int target = 0 ; target <= amount ; target++){
-            if(target % coins[0] == 0){
-                dp[0][target] = target / coins[0];
-            }else{
-                dp[0][target] = (int)Math.pow(10 , 9) + 7;
-            }
+        for(int[] ar : dp){
+            Arrays.fill(ar , -1);
         }
-        int val = tabulation(amount , coins , dp);
+        int val = recursion(n - 1 , amount , coins , dp);
         if(val == (int)Math.pow(10 , 9) + 7){
             return -1;
         }
         return val;
     }
-    // public int recursion(int index , int target , int[] a){
-    //     if(index == 0){
-    //         if(target % a[index] == 0){
-    //             return target / a[index];
-    //         }
-    //         return (int)Math.pow(10 , 9) + 7;
-    //     }
-    //     int notTake = recursion(index - 1 , target , a);
-    //     int take = (int)Math.pow(10 , 9) + 7;
-    //     if(a[index] <= target){
-    //         take = 1 + recursion(index , target - a[index] , a);
-    //     }
-    //     return Math.min(take , notTake);
-    // }
-    // public int memo(int index , int target , int[] a , int[][] dp){
-    //     if(index == 0){
-    //         if(target % a[index] == 0){
-    //             return target / a[index];
-    //         }
-    //         return (int)Math.pow(10 , 9) + 7;
-    //     }
-    //     if(dp[index][target] != -1){
-    //         return dp[index][target];
-    //     }
-    //     int notTake = memo(index - 1 , target , a , dp);
-    //     int take = (int)Math.pow(10 , 9) + 7;
-    //     if(a[index] <= target){
-    //         take = 1 + memo(index , target - a[index] , a , dp);
-    //     }
-    //     return dp[index][target] = Math.min(take , notTake);
-    // }
-    public int tabulation(int amount , int[] coins , int[][] dp){
-        for(int i = 1 ; i < coins.length ; i++){
-            for(int t = 0 ; t <= amount ; t++){
-                int notTake = dp[i -1][t];
-                int take = (int)Math.pow(10 , 9) + 7;
-                if(coins[i] <= t){
-                    take = 1 + dp[i][t - coins[i]];
-                }
-                dp[i][t] = Math.min(notTake , take);
+    public int recursion(int index , int amount , int[] coins ,int[][] dp){
+        if(index == 0){
+            if(amount % coins[0] == 0){
+                return amount / coins[0];
             }
+            return (int)Math.pow(10 , 9) + 7;
         }
-        return dp[coins.length - 1][amount];
+        if(dp[index][amount] != -1){
+            return dp[index][amount];
+        }
+        int notTake = recursion(index - 1 , amount , coins ,dp);
+        int take = (int)Math.pow(10 , 9) + 7;
+        if(coins[index] <= amount){
+            take = 1 + recursion(index , amount - coins[index] , coins , dp);
+        }
+        return dp[index][amount] = Math.min(notTake , take);
     }
 }

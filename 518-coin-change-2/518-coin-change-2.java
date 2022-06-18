a0497f0a3a5bd955fc71d0ebec7e7aefb319eb1a
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-18 18:17:23 +0530

    Time: 11 ms (48.65%), Space: 51.1 MB (29.18%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,12 +1,36 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
-        dp[0] = 1;
-        for(int i = 0 ; i < coins.length ; i++){
-            for(int j = coins[i] ; j < dp.length ; j++){
-                dp[j] += dp[j - coins[i]];
-            }
+        int ind = coins.length;
+        int[][] dp = new int[ind][amount + 1];
+        for(int[] ar : dp){
+            Arrays.fill(ar , -1);
         }
-        return dp[amount];
+        // return recursion(ind - 1 , amount , coins);
+        return memo(ind - 1 , amount , coins , dp);
+    }
+    // public int recursion(int index , int amount , int[] coins){
+    //     if(index == 0){
+    //         return (amount % coins[index] == 0 ? 1 : 0);
+    //     }
+    //     int notTake = recursion(index - 1 , amount , coins);
+    //     int take = 0;
+    //     if(coins[index] <= amount){
+    //         take = recursion(index , amount - coins[index] , coins);
+    //     }
+    //     return take + notTake;
+    // }
+    public int memo(int index , int amount , int[] coins , int[][] dp){
+        if(index == 0){
+            return (amount % coins[index] == 0 ? 1 : 0);
+        }
+        if(dp[index][amount] != -1){
+            return dp[index][amount];
+        }
+        int notTake = memo(index - 1 , amount , coins , dp);
+        int take = 0;
+        if(coins[index] <= amount){
+            take = memo(index , amount - coins[index] , coins , dp);
+        }
+        return dp[index][amount]= take + notTake;
     }
 }

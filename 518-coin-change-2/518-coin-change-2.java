5913a6b4207ef3b74aa09431da6fb58ac3bdf818
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-18 18:30:19 +0530

    Time: 16 ms (31.85%), Space: 50.9 MB (37.24%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -2,11 +2,25 @@ class Solution {
     public int change(int amount, int[] coins) {
         int ind = coins.length;
         int[][] dp = new int[ind][amount + 1];
-        for(int[] ar : dp){
-            Arrays.fill(ar , -1);
-        }
+        // for(int[] ar : dp){
+        //     Arrays.fill(ar , -1);
+        // }
         // return recursion(ind - 1 , amount , coins);
-        return memo(ind - 1 , amount , coins , dp);
+        // return memo(ind - 1 , amount , coins , dp);
+        for(int t = 0 ; t <= amount ; t++){
+            dp[0][t] = (t % coins[0] == 0) ? 1 : 0;
+        }
+        for(int index = 1 ; index < ind ; index++){
+            for(int t = 0 ; t <= amount ; t++){
+                int notTake = dp[index - 1][t];
+                int take = 0;
+                if(coins[index] <= t){
+                    take = dp[index][t - coins[index]];
+                }
+                dp[index][t] = take + notTake;
+            }
+        }
+        return dp[ind - 1][amount];
     }
     // public int recursion(int index , int amount , int[] coins){
     //     if(index == 0){
@@ -19,18 +33,18 @@ class Solution {
     //     }
     //     return take + notTake;
     // }
-    public int memo(int index , int amount , int[] coins , int[][] dp){
-        if(index == 0){
-            return (amount % coins[index] == 0 ? 1 : 0);
-        }
-        if(dp[index][amount] != -1){
-            return dp[index][amount];
-        }
-        int notTake = memo(index - 1 , amount , coins , dp);
-        int take = 0;
-        if(coins[index] <= amount){
-            take = memo(index , amount - coins[index] , coins , dp);
-        }
-        return dp[index][amount]= take + notTake;
-    }
+    // public int memo(int index , int amount , int[] coins , int[][] dp){
+    //     if(index == 0){
+    //         return (amount % coins[index] == 0 ? 1 : 0);
+    //     }
+    //     if(dp[index][amount] != -1){
+    //         return dp[index][amount];
+    //     }
+    //     int notTake = memo(index - 1 , amount , coins , dp);
+    //     int take = 0;
+    //     if(coins[index] <= amount){
+    //         take = memo(index , amount - coins[index] , coins , dp);
+    //     }
+    //     return dp[index][amount]= take + notTake;
+    // }
 }

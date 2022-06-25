8773d6f8a88fcd75ddb30990d2a0c48bbfe9d247
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-25 15:40:45 +0530

    Time: 26 ms (30.29%), Space: 46.2 MB (70.43%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -5,9 +5,19 @@ class Solution {
         for(int[] ar : dp){
             Arrays.fill(ar , -1);
         }
-        return recursion(len1 - 1 , len2 - 1 , text1 , text2 , dp);
+        // return recursion(len1 - 1 , len2 - 1 , text1 , text2);
+        return memo(len1 - 1 , len2 - 1 , text1 , text2 , dp);
     }
-    public int recursion(int ind1 , int ind2 , String t1 , String t2 , int[][] dp){
+    // public int recursion(int ind1 , int ind2 , String t1 , String t2){
+    //     if(ind1 < 0 || ind2 < 0){
+    //         return 0;
+    //     }
+    //     if(t1.charAt(ind1) == t2.charAt(ind2)){
+    //         return 1 + recursion(ind1 - 1 , ind2 - 1 , t1 , t2);
+    //     }
+    //     return 0 + Math.max(recursion(ind1 - 1 , ind2 , t1 , t2) , recursion(ind1 , ind2 - 1 , t1 , t2));
+    // }
+    public int memo(int ind1 , int ind2 , String t1 , String t2 , int[][] dp){
         if(ind1 < 0 || ind2 < 0){
             return 0;
         }
@@ -15,8 +25,8 @@ class Solution {
             return dp[ind1][ind2];
         }
         if(t1.charAt(ind1) == t2.charAt(ind2)){
-            return dp[ind1][ind2] = 1 + recursion(ind1 - 1 , ind2 - 1 , t1 , t2 , dp);
+            return dp[ind1][ind2] = 1 + memo(ind1 - 1 , ind2 - 1 , t1 , t2 , dp);
         }
-        return dp[ind1][ind2] = 0 + Math.max(recursion(ind1 - 1 , ind2 , t1 , t2 , dp) , recursion(ind1 , ind2 - 1 , t1 , t2 , dp));
+        return dp[ind1][ind2] = 0 + Math.max(memo(ind1 - 1 , ind2 , t1 , t2 , dp) , memo(ind1 , ind2 - 1 , t1 , t2 , dp));
     }
 }

7fbb4af40d6d4561ca84e17da94d4dd96c7c28c9
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-25 15:38:43 +0530

    Time: 27 ms (27.46%), Space: 46.2 MB (61.86%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,19 +1,22 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int[][] dp = new int[n + 1][m + 1];
-        for(int i = dp.length - 2 ; i >= 0 ; i--){
-            for(int j = dp[0].length - 2 ; j >= 0 ; j--){
-                char c1 = text1.charAt(i);
-                char c2 = text2.charAt(j);
-                if(c1 == c2){
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                }else{
-                    dp[i][j] = Math.max(dp[i + 1][j] , dp[i][j + 1]);
-                }
-            }
+        int len1 = text1.length() , len2 = text2.length();
+        int[][] dp = new int[len1][len2];
+        for(int[] ar : dp){
+            Arrays.fill(ar , -1);
         }
-        return dp[0][0];
+        return recursion(len1 - 1 , len2 - 1 , text1 , text2 , dp);
+    }
+    public int recursion(int ind1 , int ind2 , String t1 , String t2 , int[][] dp){
+        if(ind1 < 0 || ind2 < 0){
+            return 0;
+        }
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
+        }
+        if(t1.charAt(ind1) == t2.charAt(ind2)){
+            return dp[ind1][ind2] = 1 + recursion(ind1 - 1 , ind2 - 1 , t1 , t2 , dp);
+        }
+        return dp[ind1][ind2] = 0 + Math.max(recursion(ind1 - 1 , ind2 , t1 , t2 , dp) , recursion(ind1 , ind2 - 1 , t1 , t2 , dp));
     }
 }

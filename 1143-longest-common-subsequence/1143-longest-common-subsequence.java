ba56cf649c0e5dbc2301422e7d97c7d2081be1c5
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-25 16:00:05 +0530

    Time: 24 ms (37.08%), Space: 46.5 MB (43.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,12 +1,29 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int len1 = text1.length() , len2 = text2.length();
-        int[][] dp = new int[len1][len2];
-        for(int[] ar : dp){
-            Arrays.fill(ar , -1);
+        // int[][] dp = new int[len1][len2];
+        // for(int[] ar : dp){
+        //     Arrays.fill(ar , -1);
+        // }
+        // // return recursion(len1 - 1 , len2 - 1 , text1 , text2);
+        // return memo(len1 - 1 , len2 - 1 , text1 , text2 , dp);
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for(int j = 0 ; j <= len2 ; j++){
+            dp[0][j] = 0;
         }
-        // return recursion(len1 - 1 , len2 - 1 , text1 , text2);
-        return memo(len1 - 1 , len2 - 1 , text1 , text2 , dp);
+        for(int i = 0 ; i <= len1 ; i++){
+            dp[i][0] = 0;
+        }
+        for(int i = 1 ; i <= len1 ; i++){
+            for(int j = 1 ; j <= len2 ; j++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = Math.max(dp[i - 1][j] , dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
     }
     // public int recursion(int ind1 , int ind2 , String t1 , String t2){
     //     if(ind1 < 0 || ind2 < 0){
@@ -17,16 +34,16 @@ class Solution {
     //     }
     //     return 0 + Math.max(recursion(ind1 - 1 , ind2 , t1 , t2) , recursion(ind1 , ind2 - 1 , t1 , t2));
     // }
-    public int memo(int ind1 , int ind2 , String t1 , String t2 , int[][] dp){
-        if(ind1 < 0 || ind2 < 0){
-            return 0;
-        }
-        if(dp[ind1][ind2] != -1){
-            return dp[ind1][ind2];
-        }
-        if(t1.charAt(ind1) == t2.charAt(ind2)){
-            return dp[ind1][ind2] = 1 + memo(ind1 - 1 , ind2 - 1 , t1 , t2 , dp);
-        }
-        return dp[ind1][ind2] = 0 + Math.max(memo(ind1 - 1 , ind2 , t1 , t2 , dp) , memo(ind1 , ind2 - 1 , t1 , t2 , dp));
-    }
+    // public int memo(int ind1 , int ind2 , String t1 , String t2 , int[][] dp){
+    //     if(ind1 < 0 || ind2 < 0){
+    //         return 0;
+    //     }
+    //     if(dp[ind1][ind2] != -1){
+    //         return dp[ind1][ind2];
+    //     }
+    //     if(t1.charAt(ind1) == t2.charAt(ind2)){
+    //         return dp[ind1][ind2] = 1 + memo(ind1 - 1 , ind2 - 1 , t1 , t2 , dp);
+    //     }
+    //     return dp[ind1][ind2] = 0 + Math.max(memo(ind1 - 1 , ind2 , t1 , t2 , dp) , memo(ind1 , ind2 - 1 , t1 , t2 , dp));
+    // }
 }

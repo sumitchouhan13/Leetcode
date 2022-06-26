c4b03082db7b3acafa148c3b99e406ddda19334a
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-26 20:20:22 +0530

    Time: 3 ms (58.92%), Space: 65.4 MB (52.83%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,4 +1,6 @@
 class Solution {
+    //Explanation
+    //https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/discuss/2198002/C%2B%2B-Solution-Using-Sliding-Window
     public int maxScore(int[] cardPoints, int k) {
         int res = 0;
         for(int i= 0 ; i < k ; i++){

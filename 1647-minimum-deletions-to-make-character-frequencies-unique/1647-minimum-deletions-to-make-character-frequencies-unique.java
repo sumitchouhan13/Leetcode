9db147d939175a403fd87b55bbacb2fd6b7c916a
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-06-28 13:17:43 +0530

    Time: 12 ms (84.10%), Space: 55.1 MB (27.74%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -5,23 +5,32 @@ class Solution {
         for(char ch : s.toCharArray()){
             ar[ch - 'a']++;
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int e : ar){
-            if(e > 0){
-                pq.offer(e);
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        // for(int e : ar){
+        //     if(e > 0){
+        //         pq.offer(e);
+        //     }
+        // }
+        // while(pq.size() > 1){
+        //     int first = pq.remove();
+        //     int second = pq.remove();
+        //     if(first == second){
+        //         second--;
+        //         count++;
+        //         pq.add(first);
+        //     }
+        //     if(second > 0){
+        //         pq.add(second);
+        //     }
+        // }
+        Arrays.sort(ar);
+        int maxFreq = s.length();
+        for(int i = 25 ; i >= 0 && ar[i] > 0 ; i--){
+            if(ar[i] > maxFreq){
+                count += ar[i] - maxFreq;
+                ar[i] = maxFreq;
             }
-        }
-        while(pq.size() > 1){
-            int first = pq.remove();
-            int second = pq.remove();
-            if(first == second){
-                second--;
-                count++;
-                pq.add(first);
-            }
-            if(second > 0){
-                pq.add(second);
-            }
+            maxFreq = Math.max(0 , ar[i] - 1); 
         }
         return count;
     }

16ea90932d40ec847125284e2a8cfd29fc630d55
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-07-04 17:45:53 +0530

    Added solution - LeetHub

---
+++ Minimum steps to make product equal to one - GFG/minimum-steps-to-make-product-equal-to-one.java
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            int[] arr = new int[N];
+            
+            for(int i=0; i<N; i++)
+                arr[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.makeProductOne(arr,N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int makeProductOne(int[] arr, int N) {
+        // code here
+        int steps = 0 , zeroCount = 0 , negCount = 0;
+        for(int i = 0 ; i < N ; i++){
+            if(arr[i] > 0){
+                steps += arr[i] - 1;
+            }else if(arr[i] < 0){
+                steps += Math.abs(arr[i]) - 1;
+                negCount++;
+            }else{
+                zeroCount++;
+            }
+        }
+        if(negCount % 2 != 0 && zeroCount == 0){
+            steps += 2;
+        }
+        steps += zeroCount;
+        return steps;
+    }
+};

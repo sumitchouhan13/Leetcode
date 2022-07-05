0baef457a990630a0cb6120572fc9e84cdf6fe82
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-07-05 10:42:57 +0530

    Time: 13 ms (82.26%), Space: 55.9 MB (6.14%) - LeetHub

--- 554-brick-wall/554-brick-wall.java
+++ 554-brick-wall/554-brick-wall.java
@@ -1,12 +1,6 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
         HashMap<Integer , Integer> hm = new HashMap<>();
-        int wallSize = 0;
-        for(int j = 0 ; j < 1 ; j++){
-            for(int i = 0 ; i < wall.get(j).size() ; i++){
-                wallSize += wall.get(j).get(i);
-            }
-        }
         for(List<Integer> li : wall){
             int cut = 0;
             for(int i = 0 ; i < li.size() - 1 ; i++){

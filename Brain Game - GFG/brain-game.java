9977a41f5b00f0f7d7c00e932de23e6fc7f04f6e
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-07-07 09:04:33 +0530

    Added solution - LeetHub

---
+++ Brain Game - GFG/brain-game.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine().trim();
+            String[] S = s.split(" ");
+            int[] nums = new int[n];
+            for(int i = 0; i < n; i++){
+                nums[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            boolean ans = ob.brainGame(nums);
+            if(ans)
+                System.out.println("A");
+            else 
+                System.out.println("B");     
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public boolean brainGame(int[] nums)
+    {
+        // Code here
+        int[] ar = new int[1005];
+        for(int i = 2 ; i <= 1000 ; i++){
+            for(int j = 2 * i ; j <= 1000 ; j += i){
+                ar[j] = Math.max(ar[j] , 1 + ar[i]);
+            }
+        }
+        int x = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            x = x ^ ar[nums[i]];
+        }
+        if(x == 0){
+            return false;
+        }
+        return true;
+    }
+}

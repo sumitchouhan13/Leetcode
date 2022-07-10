2b19a4e587802eead80c51a91253f02f5f4831a8
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-07-10 13:43:13 +0530

    Added solution - LeetHub

---
+++ Find all possible paths from top to bottom - GFG/find-all-possible-paths-from-top-to-bottom.java
@@ -0,0 +1,119 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+
+class IntMatrix
+{
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException
+    {
+        int[][] mat = new int[n][];
+        
+        for(int i = 0; i < n; i++)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for(int j = 0; j < s.length; j++)
+                mat[i][j] = Integer.parseInt(s[j]);
+        }
+        
+        return mat;
+    }
+    
+    public static void print(int[][] m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+    
+    public static void print(ArrayList<ArrayList<Integer>> m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int[] a = IntArray.input(br, 2);
+            
+            
+            int[][] grid = IntMatrix.input(br, a[0], a[1]);
+            
+            Solution obj = new Solution();
+            ArrayList<ArrayList<Integer>> res = obj.findAllPossiblePaths(a[0],a[1], grid);
+            
+            IntMatrix.print(res);
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> findAllPossiblePaths(int n,int m, int[][] grid) {
+        // code here
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+        backTrack(temp , result , n , m , grid , 0 , 0);
+        return result;
+    }
+    public static void backTrack(ArrayList<Integer> temp , ArrayList<ArrayList<Integer>> result, int n , int m , int[][] grid , int i , int j){
+        ArrayList<Integer> store = new ArrayList<>();
+        store = (ArrayList)temp.clone();
+        if(i >= n || j >= m){
+            return;
+        }
+        if((i == (n - 1)) && (j == (m - 1))){
+            store.add(grid[i][j]);
+            result.add(store);
+            return;
+        }
+        store.add(grid[i][j]);
+        backTrack(store , result , n , m , grid , i + 1 , j);
+        backTrack(store , result , n , m , grid , i , j + 1);
+    }
+}
+        

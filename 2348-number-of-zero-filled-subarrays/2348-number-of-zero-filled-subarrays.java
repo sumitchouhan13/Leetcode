57b1cbd686704f8ee7b703050bf7d90cfd4fba25
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-07-25 10:17:43 +0530

    Time: 17 ms (20.00%), Space: 107.4 MB (20.00%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -0,0 +1,46 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long count = 0;
+        int n = nums.length;
+        HashMap<Long , Long> hm = new HashMap<>();
+        int i = 0;
+        while(i < n){
+            if(nums[i] == 0){
+                count++;
+            }else{
+                if(count != 0){
+                    hm.put(count , hm.getOrDefault(count , 0l) + 1);
+                }
+                count = 0;
+            }
+            i++;
+        }
+        if(count != 0){
+            hm.put(count , hm.getOrDefault(count , 0l) + 1);
+        }
+        // for(Map.Entry<Integer , Integer> map : hm.entrySet()){
+        //     System.out.println(map.getKey() + " " + map.getValue());
+        // }
+        Long max = Long.MIN_VALUE;
+        for(Long e : hm.keySet()){
+            max = Math.max(e , max);
+        }
+        if(max == Long.MIN_VALUE){
+            return 0;
+        }
+        long[] dp = new long[(int)(max + 1)];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int j = 2 ; j < dp.length ; j++){
+            dp[j] = j + dp[j - 1];
+        }
+        long ans = 0;
+        for(Map.Entry<Long , Long> map : hm.entrySet()){
+            long index = map.getKey();
+            long key = dp[(int)index];
+            long value = map.getValue();
+            ans += (key * value);
+        }
+        return ans;
+    }
+}

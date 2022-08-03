64a427a2343607feb66923445172f4e5eb9ca941
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-08-03 15:54:54 +0530

    Time: 0 ms (100.00%), Space: 39 MB (94.20%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.java
+++ 371-sum-of-two-integers/371-sum-of-two-integers.java
@@ -1,5 +1,13 @@
 class Solution {
     public int getSum(int a, int b) {
-        return a + b;
+        int carry=a & b;
+        int sum= a^b;
+        while( carry!=0){
+            carry=carry<<1;
+            int temp= sum;
+            sum= sum^carry;
+            carry= temp & carry;
+        }
+        return sum;
     }
 }

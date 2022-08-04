747f947ec0e059c32fe4e120d4ea7d9e0023f89f
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-08-04 12:33:57 +0530

    Time: 4 ms (99.99%), Space: 42.3 MB (89.87%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -2,18 +2,14 @@ class Solution {
     public String intToRoman(int num) {
         int[] intCode = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] sting = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < intCode.length; i++)
-        {
-            while(num >= intCode[i])
-            {
-                sb.append(sting[i]);
+        
+        StringBuilder res = new StringBuilder();
+        for(int i = 0 ; i < intCode.length ; i++){
+            while(num >= intCode[i]){
+                res.append(sting[i]);
                 num -= intCode[i];
             }
         }
-
-        return sb.toString();
+        return res.toString();
     }
 }

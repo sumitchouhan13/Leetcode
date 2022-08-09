f7bc322d617c9188890fa100261e75f2073b1d62
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-08-09 10:01:33 +0530

    Time: 41 ms (76.88%), Space: 47.2 MB (49.13%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.java
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.java
@@ -1,27 +1,20 @@
 class Solution {
-    public int numFactoredBinaryTrees(int[] A) {
-        int MOD = 1_000_000_007;
-        int N = A.length;
-        Arrays.sort(A);
-        long[] dp = new long[N];
-        Arrays.fill(dp, 1);
-
-        Map<Integer, Integer> index = new HashMap();
-        for (int i = 0; i < N; ++i)
-            index.put(A[i], i);
-
-        for (int i = 0; i < N; ++i)
-            for (int j = 0; j < i; ++j) {
-                if (A[i] % A[j] == 0) { // A[j] is left child
-                    int right = A[i] / A[j];
-                    if (index.containsKey(right)) {
-                        dp[i] = (dp[i] + dp[j] * dp[index.get(right)]) % MOD;
-                    }
+    public int numFactoredBinaryTrees(int[] arr) {
+        Arrays.sort(arr);
+        HashMap<Integer , Long> hm = new HashMap<>();
+        long ans = 1;
+        hm.put(arr[0] , ans);
+        int n = arr.length;
+        for(int i = 1 ; i < n ; i++){
+            long sum = 1;
+            for(int j = 0 ; j < i ; j++){
+                if(arr[i] % arr[j] == 0 && hm.containsKey(arr[i] / arr[j])){
+                    sum = sum + (hm.get(arr[i] / arr[j]) * hm.get(arr[j]));
                 }
             }
-
-        long ans = 0;
-        for (long x: dp) ans += x;
-        return (int) (ans % MOD);
+            hm.put(arr[i] , sum);
+            ans = ans + sum;
+        }
+        return (int)(ans%1000000007);
     }
 }

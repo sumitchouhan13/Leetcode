bf71eb3fce3b17ec90c8801be72fc56d44709499
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-08-29 13:42:33 +0530

    Time: 5 ms (60.00%), Space: 42.6 MB (100.00%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,17 +1,41 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
+        // Arrays.sort(nums);
+        // for(int i = 0 ; i < queries.length ; i++){
+        //     int sum = 0 , index = -1;
+        //     for(int j = 0 ; j < nums.length ; j++){
+        //         sum += nums[j];
+        //         if(sum > queries[i]){
+        //             break;
+        //         }
+        //         index = j;
+        //     }
+        //     queries[i] = index + 1;
+        // }
+        // return queries;
+        int m = queries.length, n = nums.length, i = 0;
         Arrays.sort(nums);
-        for(int i = 0 ; i < queries.length ; i++){
-            int sum = 0 , index = -1;
-            for(int j = 0 ; j < nums.length ; j++){
-                sum += nums[j];
-                if(sum > queries[i]){
-                    break;
-                }
-                index = j;
-            }
-            queries[i] = index + 1;
+        int[] prefixSum = new int[n + 1];
+        for (int num : nums) {
+            prefixSum[i + 1] = prefixSum[i++] + num;
         }
-        return queries;
+        i = 0;
+        int[] ans = new int[m];
+        for (int q : queries) {
+            ans[i++] = findIndex(prefixSum, q) - 1;
+        }
+        return ans;
+    }
+    public int findIndex(int[] nums , int toFind){
+        int low = 0 , high = nums.length;
+        while(low < high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] <= toFind){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
     }
 }

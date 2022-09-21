7890ffe55b29b31c101fc94611d646bb615e8a48
Sumit Chouhan <56264388+sumitchouhan13@users.noreply.github.com>
2022-09-21 10:54:23 +0530

    Time: 36 ms (5.47%), Space: 75.7 MB (8.04%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -1,20 +1,29 @@
 class Solution {
-    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
-        int S = 0;
-        for (int x: A)
-            if (x % 2 == 0)
-                S += x;
-
+    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int[] ans = new int[queries.length];
-
-        for (int i = 0; i < queries.length; ++i) {
-            int val = queries[i][0], index = queries[i][1];
-            if (A[index] % 2 == 0) S -= A[index];
-            A[index] += val;
-            if (A[index] % 2 == 0) S += A[index];
-            ans[i] = S;
+        int sum = arraySumMethod(nums);
+        int i = 0;
+        for(int[] query : queries){
+            int index = query[1];
+            int value = query[0];
+            if(nums[index] % 2 == 0){
+                sum -= nums[index];
+            }
+            nums[index] += value;
+            if(nums[index] % 2 == 0){
+                sum += nums[index];
+            }
+            ans[i++] = sum;
         }
-
         return ans;
+    }
+    public int arraySumMethod(int[] nums){
+        int sum = 0;
+        for(int e : nums){
+            if(e % 2 == 0){
+                sum += e;
+            }
+        }
+        return sum;
     }
 }
